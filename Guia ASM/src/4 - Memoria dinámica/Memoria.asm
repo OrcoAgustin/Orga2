1471adf41e0e4fc24cda7b0b98dbc4bcf2b1d03e
Agustin <agustinorco@gmail.com>
2026-09-07 13:10:11 -0300

    ej 3

--- Guia ASM/src/4 - Memoria dinámica/Memoria.asm
+++ Guia ASM/src/4 - Memoria dinámica/Memoria.asm
@@ -32,22 +32,6 @@ strPrint:
 
 ; uint32_t strLen(char* a)
 strLen:
-	push rbp
-	mov rbp, rsp
-
-	mov rax, 0
-	.loop:
-		cmp BYTE[rdi],0
-		je .salida
-		inc rax
-		inc rdi 
-		jmp.loop
-	.salida:
-
-	mov rsp, rbp	
-	pop rbp
-
 	ret
 
 
-; si quisies tener un indice haces mov r8,0 antes del loop y cambias en la linea cmp byte [rdi] por cmp byte [rdi+r8]
