e371319de4a2fa6195d2353a21e3377914655f61
OrcoAgustin <agustinorco@gmail.com>
2026-09-23 03:16:38 -0300

    funciona ej 1 parcial david

--- Repositorio-de-parciales-ASM-Intel-64-main/P1-ComDavid-2c2025/src/ej1/ej1.asm
+++ Repositorio-de-parciales-ASM-Intel-64-main/P1-ComDavid-2c2025/src/ej1/ej1.asm
@@ -1,5 +1,7 @@
 extern malloc
 
+extern malloc
+
 ;########### SECCION DE DATOS
 section .data
 
@@ -10,8 +12,16 @@ section .text
 USUARIO_ID_OFFSET EQU 0
 USUARIO_NIVEL_OFFSET EQU 4
 USUARIO_SIZE EQU 8
+USUARIO_NIVEL_OFFSET EQU 4
+USUARIO_SIZE EQU 8
 
 PRODUCTO_USUARIO_OFFSET EQU 0
+PRODUCTO_CATEGORIA_OFFSET EQU 8
+PRODUCTO_NOMBRE_OFFSET EQU 17
+PRODUCTO_ESTADO_OFFSET EQU 42
+PRODUCTO_PRECIO_OFFSET EQU 44
+PRODUCTO_ID_OFFSET EQU 48
+PRODUCTO_SIZE EQU 56
 PRODUCTO_CATEGORIA_OFFSET EQU 8
 PRODUCTO_NOMBRE_OFFSET EQU 17
 PRODUCTO_ESTADO_OFFSET EQU 42
@@ -22,117 +32,96 @@ PRODUCTO_SIZE EQU 56
 PUBLICACION_NEXT_OFFSET EQU 0
 PUBLICACION_VALUE_OFFSET EQU 8
 PUBLICACION_SIZE EQU 16
+PUBLICACION_VALUE_OFFSET EQU 8
+PUBLICACION_SIZE EQU 16
 
 CATALOGO_FIRST_OFFSET EQU 0
 CATALOGO_SIZE EQU 8
-;bool productoValido (producto_t*)
+
+;bool productoValido (producto*)
 global productoValido
 productoValido:
-push rbp
-mov rbp, rsp
-push r12
-sub rsp, 8
+mov r9 , [rdi+PRODUCTO_USUARIO_OFFSET]
+movzx r10, byte [r9+USUARIO_NIVEL_OFFSET]
+movzx r11, word [rdi+PRODUCTO_ESTADO_OFFSET]
 
-mov r12, rdi
+cmp r11, 1
+jne .noValido
+cmp  r10, 1
+jl .noValido
+jmp .valido
 
-movzx r9, word[r12+PRODUCTO_ESTADO_OFFSET] ;estado del producto
-mov r10, [r12+USUARIO_ID_OFFSET] ;usuario
-movzx r11, byte[r10+USUARIO_NIVEL_OFFSET] ;nivel
-cmp r9, 1
-jne .falso
-cmp r11,1
-jl .falso
-jmp .true
-
-.falso:
-    mov rax,0
-    jmp .end
-
-.true:
-    mov rax,1
-    jmp .end
-
-.end:
-    add rsp,8
-    pop r12
-    pop rbp
+.noValido:
+    mov rax, 0
     ret
 
-
+.valido:
+    mov rax, 1
+    ret
 
 ;producto_t* filtrarPublicacionesNuevasDeUsuariosVerificados (catalogo*)
 global filtrarPublicacionesNuevasDeUsuariosVerificados
 filtrarPublicacionesNuevasDeUsuariosVerificados:
+
 push rbp
 mov rbp, rsp
-push r12 ;direc de entrada
-push r13 ;publi
-push r14 ;direc res
-push r15 ;donde estamos del res
-
+push r12 ;dir de entrada
+push r13 ;primer item
+push r14 ;dir res
+push r15 ;res desplazada
+sub rsp,8
 
 mov r12, rdi
-mov r13 , [r12+CATALOGO_FIRST_OFFSET] ;la publi
+mov r13, [r12 + CATALOGO_FIRST_OFFSET]
 
 mov r10, 1
-;contador, arrancamos en 1 porque tenemos el 0 del final, 
+mov r11, r13
 
-lea r11, [r13] ;iterarlo tranqui sin perder el primero
 .recorrerRapido:
-    cmp r11,0
-    je .pedirMemo
+    cmp r11, 0
+    je .pedirMemoria
+    mov r11 , [r11 + PUBLICACION_NEXT_OFFSET]
     inc r10
-    mov r11, [r11+PUBLICACION_NEXT_OFFSET]
     jmp .recorrerRapido
 
-.pedirMemo:
-    lea r15, [r10*8]
-    lea rdi, [r10*8]
+.pedirMemoria:
+    imul r10,8
+    mov rdi, r10
     call malloc
 
-.casoVacio:
-    cmp r15,8
-    mov [rax],0
-    je .end
-
-mov r14, rax ;guardamos return 
-mov r15, r14 ;guardamos r15 el "contador" del actual 
+mov r14, rax
+mov r15, rax
 
 .loop:
     cmp r13, 0
-    je .postCiclo
-    mov rbx , [r13+PUBLICACION_VALUE_OFFSET] ;cargo producto
-    cmp [rbx],0
-    je .postCiclo
-    mov rdi, rbx
+    je .fin
+    ;13 es publicacion
+    mov rdi,[r13+PUBLICACION_VALUE_OFFSET]
     call productoValido
-.tag:
-    cmp rax, 1
-    je .agregarProd
-    jmp .preCiclo
-
-.agregarProd:    
-    mov qword[r15],rbx
-    add r15,8
-
-.preCiclo:
-    mov r13 ,[r13+PUBLICACION_NEXT_OFFSET]
-    jmp .loop
+    mov r8, rax
+    cmp r8, 1
+    je .itemValido
+    jmp .postCiclo
 
 .postCiclo:
-    mov qword[r15],0
-    mov rax, r14
-    jmp .end
+    mov r13, [r13 +PUBLICACION_NEXT_OFFSET]
+    jmp .loop
 
-.end:
+
+.itemValido:
+    mov r11, [r13+PUBLICACION_VALUE_OFFSET]
+    mov [r15], r11
+    add r15, 8
+    jmp .postCiclo
+
+
+.fin:
+    mov qword[r15], 0
+    mov rax, r14
+    add rsp, 8
     pop r15
     pop r14
     pop r13
     pop r12
     pop rbp
     ret
-
-
-
-
-
